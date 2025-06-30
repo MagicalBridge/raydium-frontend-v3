60ad25996729dcfab596cf970e02d8bc8aa6aade
louis <450793585@qq.com>
2025-06-30 09:53:05 +0800

    feat: 🎸 Add dynamic import and  comments in the Swap

--- src/pages/swap.tsx
+++ src/pages/swap.tsx
@@ -1,5 +1,6 @@
 import dynamic from 'next/dynamic'
 
+// 使用 dynamic 动态导入 Swap 组件，避免在服务器端渲染时导入
 const Swap = dynamic(() => import('@/features/Swap'))
 
 function SwapPage() {
@@ -8,6 +9,7 @@ function SwapPage() {
 
 export default SwapPage
 
+// 使用 getStaticProps 预渲染页面，避免在服务器端渲染时导入
 export async function getStaticProps() {
   return {
     props: { title: 'Swap' }
